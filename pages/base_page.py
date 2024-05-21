4019f439955936a38507906604a5824520d51691
Gribovakaterina <gribova-katerina94@yandex.ru>
2024-05-21 13:24:31 +0300

    update

---
+++ pages/base_page.py
@@ -0,0 +1,7 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope = 'function')
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install)
